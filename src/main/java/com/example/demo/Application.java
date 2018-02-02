b144850ed7e32b68650c346e16db4cfe95e07fa4
juan <juan@zipcoders-MacBook-Pro.local>
2018-02-02 15:12:42 -0500

    seventh commit

---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@SpringBootApplication
+@EnableScheduling
+public class Application {
+
+
+
+
+    public static void main(String[] args) throws Exception{
+        SpringApplication.run(Application.class);
+    }
+
+}
